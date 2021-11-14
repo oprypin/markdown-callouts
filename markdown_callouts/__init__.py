f5ed311795d0da0bbc6f50b9f0eae0491a0c3557
Oleh Prypin <oleh@pryp.in>
2021-11-14 12:02:37 +0100

    Support custom title text for admonitions
    The first bold element of the paragraph is used for that purpose.

--- markdown_callouts/__init__.py
+++ markdown_callouts/__init__.py
@@ -4,6 +4,7 @@ import xml.etree.ElementTree as etree
 from markdown import Markdown, util
 from markdown.blockprocessors import BlockQuoteProcessor
 from markdown.extensions import Extension
+from markdown.treeprocessors import Treeprocessor
 
 
 # Based on https://github.com/Python-Markdown/markdown/blob/4acb949256adc535d6e6cd84c4fb47db8dda2f46/markdown/blockprocessors.py#L277
@@ -37,12 +38,37 @@ class _CalloutsBlockProcessor(BlockQuoteProcessor):
         self.parser.state.reset()
 
 
+class _CalloutsTreeprocessor(Treeprocessor):
+    def run(self, doc):
+        for div in doc:
+            if (
+                div.tag != "div"
+                or not div.get("class", "").startswith("admonition ")
+                or len(div) < 2
+            ):
+                continue
+            title, paragraph, *_ = div
+            if title.tag != "p" or title.get("class") != "admonition-title":
+                continue
+            if paragraph.tag != "p" or len(paragraph) < 1:
+                continue
+            strong, *_ = paragraph
+            if strong.tag != "strong":
+                continue
+
+            title.text = strong.text.strip().rstrip(".")
+            if strong.tail:
+                paragraph.text = (paragraph.text or "") + strong.tail
+            paragraph.remove(strong)
+
+
 class CalloutsExtension(Extension):
     def extendMarkdown(self, md: Markdown) -> None:
         parser = md.parser  # type: ignore
         parser.blockprocessors.register(
             _CalloutsBlockProcessor(parser), "callouts", 21  # Right before blockquote
         )
+        md.treeprocessors.register(_CalloutsTreeprocessor(), "callouts", 19)  # Right after inline
 
 
 makeExtension = CalloutsExtension
