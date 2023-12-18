a679a9fd127f17d5351baf1c2f5b755a082fc121
Oleh Prypin <oleh@pryp.in>
2023-12-19 00:49:49 +0100

    Implement GitHub-style syntax

--- tests/test_extension.py
+++ tests/test_extension.py
@@ -5,12 +5,23 @@ import pytest
 from markdown import Markdown
 
 from markdown_callouts.callouts import CalloutsExtension
+from markdown_callouts.github_callouts import GitHubCalloutsExtension
+
+extension_styles = {
+    "callouts": CalloutsExtension,
+    "github": GitHubCalloutsExtension,
+}
 
 
-@pytest.mark.golden_test("extension/**/*.yml")
-def test_extension(golden):
+@pytest.mark.golden_test("callouts/**/*.yml", "github/**/*.yml", "all/**/*.yml")
+def test_extension(request, golden):
     config = {k: golden[k] for k in ["strip_period"] if golden.get(k) is not None}
-    md = Markdown(extensions=[CalloutsExtension(**config)])
+    extensions = [
+        extension(**config)
+        for key, extension in extension_styles.items()
+        if f"{key}/" in request.node.name or "all/" in request.node.name
+    ]
+    md = Markdown(extensions=extensions)
     output = md.convert(golden["input"])
     soup = bs4.BeautifulSoup(output, features="html.parser")
     html = soup.prettify().rstrip("\n")
